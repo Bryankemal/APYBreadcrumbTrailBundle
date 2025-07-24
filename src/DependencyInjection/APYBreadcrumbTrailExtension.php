3cc720d660b815bc9be7843402f30abd3ca88ba9
Bryankemal <35216106+Bryankemal@users.noreply.github.com>
2025-07-24 12:01:19 +0200

    Update APYBreadcrumbTrailExtension.php

--- src/DependencyInjection/APYBreadcrumbTrailExtension.php
+++ src/DependencyInjection/APYBreadcrumbTrailExtension.php
@@ -19,7 +19,7 @@ use Symfony\Component\HttpKernel\Kernel;
 
 class APYBreadcrumbTrailExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
