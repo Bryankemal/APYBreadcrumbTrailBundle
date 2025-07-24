5eb36e4f38db2a820c06bc0eced84859c1a3ab5b
Bryankemal <35216106+Bryankemal@users.noreply.github.com>
2025-07-24 12:02:04 +0200

    Update APYBreadcrumbTrailExtension.php

--- src/DependencyInjection/APYBreadcrumbTrailExtension.php
+++ src/DependencyInjection/APYBreadcrumbTrailExtension.php
@@ -14,7 +14,7 @@ namespace APY\BreadcrumbTrailBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\HttpKernel\Kernel;
 
 class APYBreadcrumbTrailExtension extends Extension
